e8ac88493bee2f8f1a36cde9c7588cd962d0a961
s1ovac <ilya.pereverzev123@gmail.com>
2023-02-20 16:33:45 +0300

    added logic which takes token by flag

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+)
 
+func main() {
+	t := mustToken()
+	fmt.Println(t)
+	// tgClient := telegram.New(token)
+	// fetcher := fetcher.New() - отправляет запросы, чтобы получать события
+
+	// processor := processor.New() - обрабатывает запросы
+
+	// consumer.Start(fetcher, processor)
+}
+
+func mustToken() string {
+	token := flag.String("token", "", "token to connect to TelegramApi")
+	flag.Parse()
+	if len(*token) < 1 {
+		log.Fatal(errors.New("telegram token wasn't entered"))
+	}
+	return *token
 }
